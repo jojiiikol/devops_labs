cc2c628ca0b9039bde8848673cdded4bbcd7a247
jojiiikol <jojiiikol@gmail.com>
2026-03-12 20:35:31 +0300

    terraform

---
+++ terraform-conf/yandex.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+variable "zone" {
+  type = string
+  default = "ru-central1-b"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = var.zone
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+
+provider "yandex" {
+  folder_id = "b1gcal6rpf8icohd8peg"
+  service_account_key_file = "authorized_key.json"
+  zone = var.zone
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = var.zone
+  size     = "20"
+  image_id = "fd84kd8dcu6tmnhbeebv"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+
